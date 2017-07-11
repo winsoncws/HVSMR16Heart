46882ab533a257fb08285f93d3eb75d6dd48e238
winsonhana <winson@hanalytics.sg>
2017-07-12 07:29:21 +0800

    gg

--- train.py
+++ train.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     dataset.InitDataset(splitRatio=1.0, shuffle=True)         # Take everything 100%
 #    X_ph = tf.placeholder('float32', [None, 84, 256, 256, 1])  #float32
 #    y_ph = tf.placeholder('uint8', [None, 84, 256, 256, 1])
-    X_ph = tf.placeholder('float32', [None, None, None, None, 1])  #float32
-    y_ph = tf.placeholder('uint8', [None, None, None, None, 1])
+    X_ph = tf.placeholder('float32', [None, None, None, None, 1], name='XX')  #float32
+    y_ph = tf.placeholder('uint8', [None, None, None, None, 1], name='YY')
     
     y_ph_cat = tf.one_hot(y_ph,3) # --> unstack into 3 categorical Tensor [?, 84, 256, 256, 1, 3]
     y_ph_cat = y_ph_cat[:,:,:,:,0,:]
